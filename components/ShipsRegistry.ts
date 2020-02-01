68cbd172ab53f78b6ca04a88939403689df44eaa
whtalx <whtalx@gmail.com>
2020-02-01 16:52:13 +0300

    ships placed

--- components/ShipsRegistry.ts
+++ components/ShipsRegistry.ts
@@ -9,6 +9,119 @@ interface IShip {
     }
 }
 
+const placedShips = {
+    0: {
+        numberDecksAlive: 4,
+        decks: {
+            e4: {
+                isAlive: true
+            },
+            f4: {
+                isAlive: true
+            },
+            g4: {
+                isAlive: true
+            },
+            h4: {
+                isAlive: true
+            }
+        }
+    },
+    1: {
+        numberDecksAlive: 3,
+        decks: {
+            a2: {
+                isAlive: true
+            },
+            a3: {
+                isAlive: true
+            },
+            a4: {
+                isAlive: true
+            }
+        }
+    },
+    2: {
+        numberDecksAlive: 3,
+        decks: {
+            a9: {
+                isAlive: true
+            },
+            b9: {
+                isAlive: true
+            },
+            c9: {
+                isAlive: true
+            }
+        }
+    },
+    3: {
+        numberDecksAlive: 2,
+        decks: {
+            f8: {
+                isAlive: true
+            },
+            g8: {
+                isAlive: true
+            }
+        }
+    },
+    4: {
+        numberDecksAlive: 2,
+        decks: {
+            c1: {
+                isAlive: true
+            },
+            d1: {
+                isAlive: true
+            }
+        }
+    },
+    5: {
+        numberDecksAlive: 2,
+        decks: {
+            i7: {
+                isAlive: true
+            },
+            i8: {
+                isAlive: true
+            }
+        }
+    },
+    6: {
+        numberDecksAlive: 1,
+        decks: {
+            g2: {
+                isAlive: true
+            }
+        }
+    },
+    7: {
+        numberDecksAlive: 1,
+        decks: {
+            j1: {
+                isAlive: true
+            }
+        }
+    },
+    8: {
+        numberDecksAlive: 1,
+        decks: {
+            j3: {
+                isAlive: true
+            }
+        }
+    },
+    9: {
+        numberDecksAlive: 1,
+        decks: {
+            d6: {
+                isAlive: true
+            }
+        }
+    }
+}
+
 const MAX_SHIP_LENGTH = 4;
 
 export default class ShipsRegistry {
@@ -16,27 +129,16 @@ export default class ShipsRegistry {
     public ships: {
         [key: number]: IShip,
     };
-    constructor() {
+    constructor(map) {
         let shipIndex = 0;
         this.numberShipsAlive = 10;
-        this.ships = {};
+        this.ships = { ...placedShips };
 
-        [...Array(4)].forEach((_, i) => {
-            [...Array(i + 1)].forEach(() => {
-              this.ships[shipIndex] = {
-                  numberDecksAlive: MAX_SHIP_LENGTH - i,
-                  decks: {},
-              };
-
-              [...Array(MAX_SHIP_LENGTH - i)].forEach((_, deckIndex) => {
-                  this.ships[shipIndex].decks[`a${deckIndex}`] = {
-                      isAlive: true, 
-                  };
-              });
-
-              shipIndex += 1;
-            });
-          });
+        for (let shipId in this.ships) {
+            for (let coords in this.ships[shipId].decks) {
+                map[coords].shipId = shipId;
+            }
+        }
     }
 
     public attack(id: number, coords: string) {
