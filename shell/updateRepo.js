33ec29e43021904039b3f6235b1988a0a44ec122
filgaponenko <filgaponenko@yandex-team.ru>
2020-02-01 14:03:31 +0300

    wip

--- shell/updateRepo.js
+++ shell/updateRepo.js
@@ -2,7 +2,7 @@ const exec = require('child_process').exec;
 
 function updateRepo() {
 
-    console.log('\n\n\n', 'sh ' + __dirname + '/updateRepo.sh', '\n\n\n');;
+    console.log('\n\n\n', 'sh ' + __dirname + '/shell/updateRepo.sh', '\n\n\n');;
     exec('sh ' + __dirname + '/updateRepo.sh',
     (error, stdout, stderr) => {
         console.log(stdout);
